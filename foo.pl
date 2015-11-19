2f0e782471f7decb5717b1af1a9440e028a17bac
Juerd Waalboer <juerd@tnx.nl>
2015-11-19 21:55:04 +0100

    Wrap test script in a retry loop

--- foo.pl
+++ foo.pl
@@ -5,18 +5,22 @@ use Net::MQTT;
 
 my $m = Net::MQTT.new(server => 'test.mosquitto.org');
 
-$m.connect.then: {
-    $m.subscribe("revspace/#").tap: {
-        say "REVSPACE: { .<topic> } => { .<message>.decode("utf8-c8") }";
-    }
+loop {
+    try {
+        $m.connect.then: {
+            $m.subscribe("revspace/#").tap: {
+                say "REVSPACE: { .<topic> } => { .<message>.decode("utf8-c8")}";
+            }
 
-    $m.subscribe("typing-speed-test.aoeu.eu").tap: {
-        say "Typing test completed at { .<message>.decode("utf8-c8") }";
-    }
+            $m.subscribe("typing-speed-test.aoeu.eu").tap: {
+                say "Typing test completed at { .<message>.decode("utf8-c8") }";
+            }
 
-    $m.publish("hello-world", "$*PID says hi");
-    sleep 10;
-    $m.publish("hello-world", "$*PID is still here!");
+            $m.publish("hello-world", "$*PID says hi");
+            sleep 10;
+            $m.publish("hello-world", "$*PID is still here!");
+        }
+
+        await $m.connection;
+    }
 }
-
-await $m.connection;
