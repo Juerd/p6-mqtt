dd0dc5e0e4009598b55f7296f567408b7277b61f
Juerd Waalboer <juerd@tnx.nl>
2015-12-02 05:05:39 +0100

    Simplify example code
    No retries, no try { ... }.  I still don't know how to handle
    reconnections best, but this is ugly.

--- foo.pl
+++ foo.pl
@@ -5,22 +5,18 @@ use Net::MQTT;
 
 my $m = Net::MQTT.new(server => 'test.mosquitto.org');
 
-loop {
-    try {
-        $m.connect.then: {
-            $m.subscribe("revspace/#").tap: {
-                say "REVSPACE: { .<topic> } => { .<message>.decode("utf8-c8")}";
-            }
-
-            $m.subscribe("typing-speed-test.aoeu.eu").tap: {
-                say "Typing test completed at { .<message>.decode("utf8-c8") }";
-            }
-
-            $m.publish("hello-world", "$*PID says hi");
-            sleep 10;
-            $m.publish("hello-world", "$*PID is still here!");
-        }
-
-        await $m.connection;
+$m.connect.then: {
+    $m.subscribe("revspace/#").tap: {
+        say "REVSPACE: { .<topic> } => { .<message>.decode("utf8-c8")}";
     }
+
+    $m.subscribe("typing-speed-test.aoeu.eu").tap: {
+        say "Typing test completed at { .<message>.decode("utf8-c8") }";
+    }
+
+    $m.publish("hello-world", "$*PID says hi");
+    sleep 10;
+    $m.publish("hello-world", "$*PID is still here!");
 }
+
+await $m.connection;
